15653e57ad2bfbad138e2ee7000ff311e10c592f
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-06-01 20:56:56 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,8 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    print("Hello from Lambda!")
-    return {
+     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
