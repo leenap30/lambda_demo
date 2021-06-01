5a32263a9d6398ac91425968b344b3580fcf23c0
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-06-01 20:44:17 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,2 +1,8 @@
-def hello(event, context):
-  print("Welcome to terraform")
+import json
+
+def lambda_handler(event, context):
+    print("Hello from Lambda!"
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
