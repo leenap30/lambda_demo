9bdbb381a3425def78299184800149c91ec318af
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-06-01 21:28:59 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,8 +1,12 @@
 import json
 
 def hello(event, context):
+        urls = ["https://youtu.be/EBSdyoO3goc",
+                "https://youtu.be/su9VDwCrCos",
+                "https://youtu.be/4L-mJc3XakU"];
+    
     print("Hello from Lambda!")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.stringify(urls)
     }
