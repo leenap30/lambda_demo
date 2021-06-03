a318a119ca80e230d0a2a88be8aab3704100189e
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-06-03 14:12:40 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,8 +1,8 @@
 exports.handler = async (event) => {
    
-    var urls = ["https://youtu.be/EBSdyoO3goc",
-                "https://youtu.be/su9VDwCrCos",
-                "https://youtu.be/4L-mJc3XakU"];
+    var urls = ["https://youtu.be/embed/EBSdyoO3goc",
+                "https://youtu.be/embed/su9VDwCrCos",
+                "https://youtu.be/embed/4L-mJc3XakU"];
      const response = {
         statusCode: 200,
         body: JSON.stringify(urls),
